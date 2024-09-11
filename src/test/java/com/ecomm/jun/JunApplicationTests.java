c776d62388568f8765ffbab0398a3baeffb4eec3
furkankizilbuga <furkankizilbuga12@gmail.com>
2024-09-11 15:19:58 +0300

    Initial commit

---
+++ src/test/java/com/ecomm/jun/JunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomm.jun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
